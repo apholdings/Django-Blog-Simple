ebf2057c83fd2463e97d7426f37990e2f548b2e6
NineRogues <alexander.eric.3@gmail.com>
2021-08-14 13:27:42 -0500

    vista about

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
-from .views import HomeView
+from .views import HomeView, AboutView, ContactView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact")
 
 ]
 if settings.DEBUG:
--- core/views.py
+++ core/views.py
@@ -5,6 +5,21 @@ from django.views.generic import View
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         context={
-            
+
         }
         return render(request, 'index.html', context)
+
+class AboutView(View):
+    def get(self, request, *args, **kwargs):
+        context={
+            
+        }
+        return render(request, 'about.html', context)
+
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        context={
+            
+        }
+        return render(request, 'contact.html', context)
