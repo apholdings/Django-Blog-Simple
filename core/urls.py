b7a21185aabd6e6d901b56871d2e87eb1b7dad1e
NineRogues <alexander.eric.3@gmail.com>
2021-08-14 12:53:25 -0500

    vista home

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from .views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home")
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
