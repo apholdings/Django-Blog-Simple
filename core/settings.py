d02a4b9b2bda43a192f86d3cfca8c12e3fdf7ae7
csais3 <108033096+csais3@users.noreply.github.com>
2023-02-22 08:37:39 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
 
     'newsletters',
     'dashboard',
+    
+    'storages',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -158,4 +160,23 @@ SECURE_REDIRECT_EXEMPT = []
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+AWS_ACCESS_KEY_ID= env('AWS_ACCESS_KEY_ID')
+SECRET_ACCESS_KEY= env('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= env('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
+
+STATIC_LOCATION = 'static'
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
